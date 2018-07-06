6e07f9727c0d1f862f78e3094bb2a92289adb389
qwn <qwn@qwns-MacBook-Air.local>
2018-07-06 09:15:42 +0530

    Day 5 : horrible! can't even code a calculator

--- Day4/app/src/main/java/com/example/qwn/day4/MainActivity.java
+++ Day4/app/src/main/java/com/example/qwn/day4/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.qwn.day4;
 
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     private long result = 0;
+    private long number = 0;
+    private String oper = null;
     private String logTag = "MainActivity";
 
     @Override
@@ -20,29 +23,59 @@ public class MainActivity extends AppCompatActivity {
     public void OperatorClicked(View v)
     {
         EditText editText = findViewById(R.id.textBox);
-        long val = Long.parseLong(editText.getText().toString());
+        if(oper != null){
+            result = PerformOperation(oper, number, result);
+            oper = null;
+            number = 0;
+            Log.v(logTag, "result " + result);
+        }
+        number = Long.parseLong(editText.getText().toString());
         editText.setText("");
         switch(v.getId()){
             case R.id.add : {
-                result += val;
+                oper = "Add";
                 Log.v(logTag, "Add Clicked");
             }break;
             case R.id.sub : {
-                result += val;
+                oper = "Sub";
                 Log.v(logTag, "Sub Clicked");
             }break;
             case R.id.mul : {
-                result += val;
+                oper = "Mul";
                 Log.v(logTag, "Mul Clicked");
             }break;
             case R.id.div : {
-                result += val;
+                oper = "Div";
                 Log.v(logTag, "Div Clicked");
             }break;
             case R.id.cal: {
+                if(oper != null) {
+                    result = PerformOperation(oper, number, result);
+                    oper = null;
+                }
+                editText.setText(Long.toString(result));
                 Log.v(logTag, "Cal click " + v.getId());
             }break;
         }
-        editText.setText(Long.toString(result));
+
+    }
+
+    private long PerformOperation(String operation, long val, long result)
+    {
+        switch(operation){
+            case "Add":{
+                result += val;
+            }break;
+            case "Mul":{
+                result *= val;
+            }break;
+            case "Div":{
+                result /= val;
+            }break;
+            case "Sub":{
+                result -= val;
+            }break;
+        }
+        return result;
     }
 }
